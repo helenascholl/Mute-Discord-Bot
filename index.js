e20fe49a94dfb378cd3a98a5ec8a66d47f5fa071
schollsebastian <scholl.sebastian@gmx.at>
2021-02-16 14:03:23 +0100

    Implement !m help

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message.content);
+    if (message.content.startsWith('!m')) {
+        const command = message.content.split('!m ')[1];
+
+        switch (command) {
+            case 'help':
+                help(message);
+                break;
+        }
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN).catch(console.error);
+
+function help(message) {
+    const help = '`!m mute @Member:` Vote to mute a member\n'
+        + '`!m deafen @Member:` Vote to deafen a member\n'
+        + '`!m unmute @Member:` Vote to unmute a member\n'
+        + '`!m undeafen @Member:` Vote to undeafen a member';
+
+    const embed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Mute-Bot')
+    .setURL('https://github.com/schollsebastian/Mute-Discord-Bot')
+    .addField('Help', help);
+
+    message.channel.send(embed).catch(console.error);
+}
